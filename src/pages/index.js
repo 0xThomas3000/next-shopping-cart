cdd67894f056e668640f87995d90c6022cda318e
tuanit168 <tuanit168@gmail.com>
2022-06-12 08:07:38 +0700

    Setup project with some lib

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookSkull } from "@fortawesome/free-solid-svg-icons";
+
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world
+    </h1>
+  );
+}
