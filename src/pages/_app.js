c1aade1e2bf02912dc525b54adacc15f8ea4ed8d
tuanit168 <tuanit168@gmail.com>
2022-06-13 15:07:03 +0700

    Demo children prop

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,11 @@
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Component {...pageProps} name="Tuan" age={35}>
+      <h1 className="font-bold text-3xl">This is a &quot;Children prop&quot;</h1>
+    </Component>
+  );
 }
 
 export default MyApp;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,23 @@
-export default function Home() {
+import { useState } from "react";
+
+export default function Home({ name = "Linh", age, children }) {
+  const [products, setProducts] = useState(1);
   return (
     <div>
-      This is a Home Page Component
+      <div>Name: {name}</div>
+      <div>Age: {age}</div>
+      <div>Home: {children}</div>
+      <h1 className="font-bold text-3xl text-red-500">{products}</h1>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => {
+          let products = "A";
+          setProducts((prev) => prev + 1);
+          // console.log(products)
+        }}
+      >
+        Click
+      </button>
     </div>
   );
 }
