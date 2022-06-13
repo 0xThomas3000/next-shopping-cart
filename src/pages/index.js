7067c2ec59dc7ba01c30dbb976aa3452a128d4ec
tuanit168 <tuanit168@gmail.com>
2022-06-13 14:14:15 +0700

    Finish implementing a basic Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,7 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBookSkull } from "@fortawesome/free-solid-svg-icons";
-
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world
-    </h1>
+    <div>
+      This is a Home Page Component
+    </div>
   );
 }
